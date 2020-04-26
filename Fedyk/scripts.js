e124f0fe4e285e1e01ac8828d23fe18da6d0ae9a
Nambarc <25438361+Nambarc@users.noreply.github.com>
2020-04-26 18:09:39 +0100

    Fedyks part

---
+++ Fedyk/scripts.js
@@ -0,0 +1,48 @@
+
+// ----------------------------------------------------------------------------
+// BLOCK 1
+let block_1_cipher_text = "U2FsdGVkX1+Kmy/IOnRAVZDOkjayZZjIeIYI0FHI4BTt0n7bEiu16M95Jl+za57SGXcUz1ZSWYqVXt2HtRexCoiJIWtqDOfkV5mrnGQuS0bwyRYOfB2syjH8wqS5TEy460JnSsWyb2TgVzFak1w0SAK1rEw4/LA0lTenUIOwhjPoQxigiMe3yIU3nJQu7l+HsoFqG+0ioGTYLK1Hrl0J2oqAC9Ok/dHiCwddoXc2L+BLyVrt46KYoSc0Imut6lFGZoC++DzvGhMCstFnj0FruMXels+UYrirdTuui3gOBLik26ppCw2MRtcQ0MjbIxkDwLWQqqdYKoyNbU28YF0kE8w8SF3Jdzqfq1lvlQhk99zJ+fX4iEvCggyCJM6qINiwNFlPE2X4bUpISPAZAc8j/nRbraXMCXpGzhQU8odiIpKL5qA8C0+gR0/kTys1SlgL00W4sa84FL7qzPxUbm0RcJGfNZ49YNU4dm/dRD9AOWCeqd2llsB/bNIrmGxsNiq0YKtVFAgvR2eFYgj095fIvFSV/8Iny2mzZ41N3BqnyWy3zM93aZUZq2xRC9eDw2gvrTF32dp3h363qU86LGxRLQJtZI3MKpjhL5odxsuC97q/iCMDxiaNv+VW0LyD9DYgqze4tsir0BeQmqhDMJzCAGdg7rnLURkGHcES2+9QDB03RsgiGWWlwaXEqZaY5/f8ZPLPTJka5vIvTNTkzOJwsdHo+r68trQwVmDy9QpwerenRc6sm65AiivpqHuhVWDqHdtIy6QTopu44N/i8D5djnoO66+Bc2QFr60X6dGwxmfpwHrTf/01QgZ2Ujjk1SJN5YHMiv3FNw7tf74JKLECBcybkJXm8xKS0igMCLDykW5VEPQTnRqPZdpyRh9GYbjiZpmRADixNiAu0R/y7yKcuVH/fAN2x2FQJ8ZxjqlzfXhJBeT7p3dRW24beOZ11NIq8CILoseOwX1LcntntE8WK5nHPtp7Z7CIbULAM6yM9XbHpEDlVFGPSUrKzR13Lpp3rddvR9RtwdUjZkKJHD+smnxfiuzpY9isQ/21rdaMFImrkbqpVev6L5uT4GvWtgeGP5qdQ6cWcf+JFqfwoywAWo39EHPCE4XrqxSgL7rKWu5HYJp5bGP3w3udhjtYMHLLpftRxm/MtLsrsr5T2hEaohn3xZCvDsTiESaXSIxvuFl9GFV6LellN2TE/ZWxhXHbrwcvIXZSnTt28wsgyQmp1L8+PiKIUoUIalmnAmYwhtJPUUVYLwr+x9R1YTqh285kkqrqMwb8vMJi6yASabF64fPxdmRpnr+dYOIR/ong+r+0F97k7q0piDPGuNjJQicm8s1v5MpB4skb2dS457P76lnq4Cm2cR2WU+59Y2v3PhjHDk3eJRfdUUqenBmtUj1JKdJSVJRXGdw6y6nUnP83ESG9dCyK28toocRX/8ZJlU1oNx2EHWDTsgv7jGjJilejJdjeIBM62anJ03F3lpHlVVZkWZGAJdZ/7cfQHhVCjNovYroHQ5A0nWH/EgvML9k/0Srg0isAECnmjUOxq1PS7HWzz770GnToGO3/IAU52RSg3BMdY2ic6HmRZos3jntqLS7nXkhbiqxp7/kIVN9fQ/YoQNhUqNjMSDjIfS9Wz6seZuWd1yr1aH2qRX22fgaKe+AV4p67aT1t8vwGdtCs3eBIS48zzHE8lfUPL0VYGlraD0vbPCdFwo8g6KnbiZOByxu38zpzrIY8XfXfcXTVsx33ml7mB6BRN2we6+XEsl163sciG4C+fNxyQMWtiUxi8So99B0gEDWlyXYF76w0OrQX4aE+T8dVE/ME7XYoOgZR5h2Gjhw/ox58y986cjqZZMa0rjdWJf5UgPIna2kI40oTli73uiT5AVIE2nd6xFGSjI/lvpbDJN0Bt6WKTXAztwJ5dAnvvDLiKrlB6pFONS6he1lp8UxMM4zPc1RpqyJLMMmpczqbx1JNv05wiixuYtlj0elgO7fSIue0MNdYw8nXdWrKr4wuHP+5kZOM4R5zOHVsH9pdAM7UcofkHqtWLDb5IGLJjy7yWLjQ7pUnDy3WGkivNpNUw9fGojDcs3gjBGge4PeLaBuqD1IqaFPYNrLLyNkJoF8yYTAllNQdh60LC8PS282Or4EwyM+EVtc8cA1luAXHFG7NvkZ0sWsdabdzHybzlwk8VRav/38e8GY+btzAveOeg/VYnyPYIG09SAoAnzXSCOZtL5m5CkC/M8eYhmnlmLOHwUKNKruVJd9C0qWK5w7AfdkXpFIyh8xMoNA+PWWC8OAL6/GKTKX1aemg5Y5uo3k5eseIOMOCZ4GxZb4ShHKzbghXmrUn6+h8kDfnHGYC01YweHiMMRdoTr7m2WFO9Ph34pnBIdYg4rE2y1akbLXMbMCcEnPo8ERSF4HESqD0sJ/deAd7WguVtHG6FRrlHIyZJ7vP7Vt2/lMynT+vJ64lQ7UaVeVSdqs+ORaqjXf7VJf8Vb/ppQWNN5L4CSMqZ9MIV6h6O3KHHjMtqKJLPvRydPwgWIf+pCgKARl7lBGXJwHD+E9V3jXeA3bbYe2XxmRIwmT7csW0HwFhLxKyUA57sZgUWE4wZCog8z8MEgf2Lhv+6+4DGn5YDsWDpH91bymnGi/Nr9+bVMqLbAXRszE6t55rU+uOKyeBZUyxDXfKTi3AQtMeo8fR4p9tZj59HRQIczNU/IekmFlVxkcMZ6eUwBwREUuTfjwWMxFmh68J+D3Td5AsDYTAa6vR33r46uFQGvUxS6XZ7jYz3rCW/OTi9GxtjcZx3dCdQDNBANDJjqDRuWJNamrDmRaQLYcXWFrkuL4OvWJtbnPmCSlQwybQX8eV0SqGazBb2e/8hjvOz0LmvmZQDMgBvDSb8/kwVPyvB2CyH8iDuQUIPX/rxFfFWG1yx4XG23du74ZKofwj8ryO3B0QgdPWHde2X5JJDvStYlrU+1CfET2Xw/HtYaGLrJDwEUW+L2f6Z07tnOZh93qgkCyaCoSGpRekV2jWrxwFW9sjkfFQvl6wPdOtFjwdE6dfO/py2urkTlYJPRqKYh2lfB4ZI5Ge5uR53v7gBh9fEpgiZz9MvBa1DYCkRaNcWOP1LW8Vieo3YdXCygEw2keNXhmxFfUCThPCSEChfrOQDSZ2sEA5iW48pndQK+bAZJktHgGdP/YzR1BeP0nAUZSp09VJOrnmr3O+GaDrHeeSgDiObd4BCs6Av0sFWhALNRuZIt6wybdGnqRJImoER+EgCDYdlyrm6d95zWPQX1dwuvGo+8dSAdD2nItLCgUf/DT6yrqC1BfsgbAhRsAS6y26AWnT3OYRGOjOSFXrZqVcgIABB1Q8lfX4ouDUpOiI6pXlsU9tnY+NKw1IR35oWxFot07QWTY5D2lujXQYIyr9LR13ZULo118KqituEcNClKvNdh+5SkvCnXWrBE3hTYkud+mu6ZU3Id+hj/B1g8AhmKafzLt6z8LHrww99HfrSxH7G4ssPVB7/bjLx3EoQW2YQtXvo6VXlw3wIQeXR9uSsPpGVn/QJXNP0FZvt9lzkI8cD8RYT93hpVM2PKRtgN9q4///KY7GAWz6QXfhFjcLYw2q0BNwHQ0QSOldX3Eawl3R6HzigYJ/XQfpk7EJgA9Qf856FSN5KsjkuQsXOw+C/iVdjyB46wl5rlMvEKZWiUJ5++V1sq4=";
+let block_1 = document.getElementById("block_1");
+let block_1_password_entry = document.getElementById("block_1_password_entry");
+let block_1_decode_button = document.getElementById("block_1_decode_button");
+let block_1_taunt = document.getElementById("block_1_taunt");
+
+block_1_decode_button.addEventListener("click", function () {
+  let password = block_1_password_entry.value;
+  let decrypted_text = Decode(block_1_cipher_text, password);
+  if (decrypted_text == "fail") {
+    block_1_taunt.innerText = "Did you try to guess...? You cheeky bugger!";
+  } else {
+    block_1.innerHTML = decrypted_text;
+  }
+});
+// ----------------------------------------------------------------------------
+
+// Function to decode an encrypted block of text given the correct password.
+function Decode(cipher_text, password) {
+
+  // Decrypt the text using the provided password.
+  let original_bytes = CryptoJS.AES.decrypt(cipher_text, password);
+
+  // Try to interpret the decrypted bytes as a UTF-8 string.
+  let decrypted_text = "fail";
+  try {
+    decrypted_text = original_bytes.toString(CryptoJS.enc.Utf8);
+  } catch (error) {
+    console.log(error);
+  }
+  return decrypted_text;
+}
+
+// Function to encode a raw block of text into cipher text with the given password.
+function Encode(raw_text, password) {
+  let encrypted_text = CryptoJS.AES.encrypt(raw_text, password).toString();
+  return encrypted_text;
+}
+
+let raw_text = `
+
+`;
+let password = "";
+let encrypted_text = Encode(raw_text, password);
+console.log(encrypted_text);
